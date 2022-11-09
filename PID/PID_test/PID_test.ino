8d81a39eeebcf4afe961cf570b69955d597c82d7
HyeonHan Bae <baehyunhan3@gmail.com>
2022-11-09 14:00:07 +0900

    Create PID_test.ino

---
+++ PID/PID_test/PID_test.ino
@@ -0,0 +1,33 @@
+/**
+ * PID 구성 식
+ * plotter 사용 바람
+*/
+float Result = 0;
+float oneTimeDelay = 20; // [mS]
+
+float pre_P_error = 0;
+float P_error, I_error, D_error;
+float PID(float Goals, float Kp, float Ki, float Kd)
+{
+    P_error = Goals - Result; // 목표값과 실제값 차이
+    I_error += P_error;
+    D_error = pre_P_error - P_error;
+    return ( P_error * Kp ) + ( I_error * Ki ) + ( D_error * Kd );
+}
+
+void setup()
+{
+    Serial.begin(115200);
+}
+
+float Goals;
+void loop()
+{
+    if(Serial.available()){
+        String text = Serial.readString();
+        Goals = text.toFloat();
+    }
+    Result = PID(Goals,0.1,0.1,0.2);
+    Serial.println(Result);
+    delay(oneTimeDelay);
+}
